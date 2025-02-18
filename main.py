129944f545fc8a1c2d57030d2c2b230002fbc22f
Dr4mchik <denis.desh12343@mail.ru>
2025-02-18 16:49:39 +0300

    HOT FIX DOLLAR $ -> &, comment

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import requests
 
 server_address = 'https://static-maps.yandex.ru/v1?'
 api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-ll_one = 37.530887
-ll_two = 55.703118
-spn_one = 0.002
-spn_two = 0.002
+ll_one = 37.530887 # Долгота
+ll_two = 55.703118  # Широта
+spn_one = 0.002     # Разность долгота
+spn_two = 0.002     # Разность широта
 
 map_request = f"{server_address}ll={str(ll_one)},{str(ll_two)}&spn={str(spn_one)},{str(spn_two)}&apikey={api_key}"
 response = requests.get(map_request)
@@ -25,7 +25,7 @@ with open(map_file, "wb") as file:
     file.write(response.content)
 
 pygame.init()
-screen = pygame.display.set_mode((600, 500))
+screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
