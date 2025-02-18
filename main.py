c6b51323559ecb4dc7bbae6d29e0ee6f5aaf669d
Dr4mchik <denis.desh12343@mail.ru>
2025-02-18 17:44:33 +0300

    zoom map

--- main.py
+++ main.py
@@ -6,30 +6,52 @@ import requests
 
 server_address = 'https://static-maps.yandex.ru/v1?'
 api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-ll_one = 37.530887 # Долгота
+ll_one = 37.530887  # Долгота
 ll_two = 55.703118  # Широта
-spn_one = 0.002     # Разность долгота
-spn_two = 0.002     # Разность широта
-
-map_request = f"{server_address}ll={str(ll_one)},{str(ll_two)}&spn={str(spn_one)},{str(spn_two)}&apikey={api_key}"
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
+spn_one = 0.002  # Разность долгота
+spn_two = 0.002  # Разность широта
 map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+
+
+def request():
+    map_request = f"{server_address}ll={str(ll_one)},{str(ll_two)}&spn={str(spn_one)},{str(spn_two)}&apikey={api_key}"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
 
 pygame.init()
+running = True
 screen = pygame.display.set_mode((600, 450))
+request()
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                spn_one -= spn_one / 2
+                spn_two -= spn_two / 2
+                if spn_one < 0.0001 or spn_two < 0.0001:
+                    spn_one = 0.0001
+                    spn_two = 0.0001
+                request()
 
+            elif event.key == pygame.K_s:
+                spn_one += spn_one
+                spn_two += spn_two
+                request()
+
+            screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
 os.remove(map_file)
